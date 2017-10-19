0cfaf29c49d3aaab3cb4231efdcf9f7a7ea54b6b
ykeanu <ykhernan@ucla.edu>
2017-10-18 21:05:35 -0700

    Creating components with createClass()

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { SkiDayCount } from './components/SkiDayCount'
+
+// Adding react to the window prevents react from being undefined
+window.React = React
+
+render(
+	<SkiDayCount />,
+	document.getElementById('react-container')
+)
